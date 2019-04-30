f65fd6297fad776b092e509ba69e8bf5104c7859
PikaJoyce <pikajoyce@gmail.com>
2019-04-30 15:22:38 -0700

    Implemented CRUD functions.

--- datastore/index.js
+++ datastore/index.js
@@ -13,83 +13,79 @@ var items = {};
 //  
 
 exports.create = (text, callback) => {
-
-  //console.log(location)
-  counter.getNextUniqueId((err,id) => {
-    //need to have error ok???????????
-    //ni xu yao error
+  counter.getNextUniqueId((err, id) => {
+    if (err) {
+      return callback(err);
+    };
     items[id] = text;
-
     var location = path.join(exports.dataDir, `${id}.txt`);
     fs.writeFile(location, text, (err) => {
-      if(err){
+      if (err) {
         callback(err);
       } else {
-        callback(null, {id, text})
-      }
+        callback(null, { id, text })
+      };
     });
-  })
-}
-//var location = path.join(exports.dataDir, `${id}.txt`);
+  });
+};
+
 
 exports.readAll = (callback) => {
   fs.readdir(exports.dataDir, (err, files) => {
-    if(err){
-      callback(err)
+    if (err) {
+      callback(err);
     } else {
-      var data = _.map(files, (file,text, id) => {
-         id = file.slice(0, -4);
-         text = id;
-         return {id, text};
-        })
-        callback(null, data);
-      }
-  })
-}
+      var data = _.map(files, (file, text, id) => {
+        id = file.slice(0, -4);
+        //REFACTOR THIS
+        text = id;
+        return { id, text };
+      });
+      callback(null, data);
+    };
+  });
+};
+//var location = path.join(exports.dataDir, `${id}.txt`);
 
 exports.readOne = (id, callback) => {
   var location = path.join(exports.dataDir, `${id}.txt`);
   fs.readFile(location, (err, data) => {
-    if(err){
+    if (err) {
       callback(err);
     } else {
-      var todo = {id, text: data.toString()}
-     
-      callback(null, todo)
-    }
-  })
+      var text = data.toString()
+      var toDo = { id, text }
+      callback(null, toDo)
+    };
+  });
 };
 
 exports.update = (id, text, callback) => {
-
   var location = path.join(exports.dataDir, `${id}.txt`);
   fs.access(location, (err) => {
-    if(err){
-      callback(err)
+    if (err) {
+      callback(err);
     } else {
       fs.writeFile(location, text, (err) => {
-        if(err){
+        if (err) {
           callback(err);
         } else {
-          callback(null, {id, text})
-        }
+          callback(null, { id, text });
+        };
       });
-    }
-  })
-  
-}
-
+    };
+  });
+};
 
 exports.delete = (id, callback) => {
   var location = path.join(exports.dataDir, `${id}.txt`);
   fs.unlink(location, (err) => {
-    if(err){
+    if (err) {
       callback(err);
     } else {
-      callback(location);
-    }
-  })
-ß
+      callback(null, location);
+    };
+  });
 };
 
 // Config+Initialization code -- DO NOT MODIFY /////////////////////////////////
